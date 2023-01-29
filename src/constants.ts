79e5f5f809e7952fc7a2d794c27597f2fbdf1b86
kzhrk <info@kzhrk.com>
2023-01-29 17:15:27 +0900

    add core function

---
+++ src/constants.ts
@@ -0,0 +1,46 @@
+import { WarekiError } from '@/index';
+
+type Wareki = 'meiji' | 'taisho' | 'showa' | 'heisei' | 'reiwa';
+type WarekiJp = '明治' | '大正' | '昭和' | '平成' | '令和';
+type WarekiObject = {
+  firstDate: Date;
+  firstYear: number;
+  jp: WarekiJp;
+};
+
+export const MILLISECONDS_PER_DAY = 24 * 60 * 60 * 1000;
+
+export const WAREKI: Readonly<Record<Wareki, WarekiObject>> = {
+  meiji: {
+    firstDate: new Date('1868-01-25'),
+    firstYear: 1868,
+    jp: '明治',
+  },
+  taisho: {
+    firstDate: new Date('1912-07-30'),
+    firstYear: 1912,
+    jp: '大正',
+  },
+  showa: {
+    firstDate: new Date('1926-12-25'),
+    firstYear: 1926,
+    jp: '昭和',
+  },
+  heisei: {
+    firstDate: new Date('1989-01-08'),
+    firstYear: 1989,
+    jp: '平成',
+  },
+  reiwa: {
+    firstDate: new Date('2019-05-01'),
+    firstYear: 2019,
+    jp: '令和',
+  },
+};
+
+export const ERROR: Readonly<Record<string, WarekiError>> = {
+  invalidParams: {
+    status: 422,
+    message: 'invalid params',
+  },
+};
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,96 @@
-export function test() {
-  const i = 1;
-  console.log(i);
+import { ERROR, WAREKI } from '@/constants';
+import {
+  convertJapaneseEraToWesternCalendar,
+  convertOneToGen,
+} from '@/converter';
+import {
+  checkHeisei,
+  checkMeiji,
+  checkReiwa,
+  checkShowa,
+  checkTaisho,
+} from '@/validation';
+
+export type WarekiError = {
+  status: number;
+  message: string;
+};
+
+function convertJapaneseEraToISO8601(wareki: string): string | WarekiError {
+  const regexpWareki = /^(.+)年(.+)月(.+)日$/;
+  const matchesWareki = wareki.match(regexpWareki);
+
+  if (matchesWareki.length === 4) {
+    const [, japaneseEra, month, day] = matchesWareki;
+    const regexpJapaneseEra = new RegExp('(明治|大正|昭和|平成|令和)(.+)');
+    const [, era, yearStr] = japaneseEra.match(regexpJapaneseEra);
+
+    if (era && yearStr) {
+      const year = convertJapaneseEraToWesternCalendar(japaneseEra);
+
+      if (year) {
+        const dateISO8601 = `${year}-${month.padStart(2, '0')}-${day.padStart(
+          2,
+          '0',
+        )}`;
+
+        const isMeiji =
+          era === WAREKI.meiji.jp && checkMeiji(new Date(dateISO8601));
+        const isTaisho =
+          era === WAREKI.taisho.jp && checkTaisho(new Date(dateISO8601));
+        const isShowa =
+          era === WAREKI.showa.jp && checkShowa(new Date(dateISO8601));
+        const isHeisei =
+          era === WAREKI.heisei.jp && checkHeisei(new Date(dateISO8601));
+        const isReiwa =
+          era === WAREKI.reiwa.jp && checkReiwa(new Date(dateISO8601));
+
+        if (isMeiji || isTaisho || isShowa || isHeisei || isReiwa) {
+          return dateISO8601;
+        }
+      }
+    }
+  }
+
+  return ERROR.invalidParams;
+}
+
+function convertWesternCalendarToJapaneseEra(date: Date): string | WarekiError {
+  let year: string;
+
+  if (checkMeiji(date)) {
+    year = `${WAREKI.meiji.jp}${convertOneToGen(
+      date.getFullYear() - WAREKI.meiji.firstDate.getFullYear() + 1,
+    )}`;
+  } else if (checkTaisho(date)) {
+    year = `${WAREKI.taisho.jp}${convertOneToGen(
+      date.getFullYear() - WAREKI.taisho.firstDate.getFullYear() + 1,
+    )}`;
+  } else if (checkShowa(date)) {
+    year = `${WAREKI.showa.jp}${convertOneToGen(
+      date.getFullYear() - WAREKI.showa.firstDate.getFullYear() + 1,
+    )}`;
+  } else if (checkHeisei(date)) {
+    year = `${WAREKI.heisei.jp}${convertOneToGen(
+      date.getFullYear() - WAREKI.heisei.firstDate.getFullYear() + 1,
+    )}`;
+  } else if (checkReiwa(date)) {
+    year = `${WAREKI.reiwa.jp}${convertOneToGen(
+      date.getFullYear() - WAREKI.reiwa.firstDate.getFullYear() + 1,
+    )}`;
+  }
+
+  if (year) {
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    return `${year}年${month}月${day}日`;
+  } else {
+    return ERROR.invalidParams;
+  }
+}
+
+export function warekiUtils(target: Date | string) {
+  return typeof target === 'string'
+    ? convertJapaneseEraToISO8601(target)
+    : convertWesternCalendarToJapaneseEra(target);
 }
